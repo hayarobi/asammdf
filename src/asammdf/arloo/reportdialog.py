cef59df02f55ff2fcb9ac90a227431348f6ff951
Hayarobi Park <hayarobipark@gmail.com>
2023-04-26 23:09:17 +0900

    Revert "리포트 이미지 크기 조정 기능 초안 추가"
    This reverts commit df68b0e5f21974431c2a7c2b78b4345995bb1ee0.
    
    - 다른 개체의 사이즈가 비정상이 되는 부작용이 있어서 일단 보류함

--- src/asammdf/arloo/reportdialog.py
+++ src/asammdf/arloo/reportdialog.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-from pathlib import Path
+from pydoc import html
 
 from PySide6 import QtCore, QtWidgets
 from PySide6.QtCore import QDateTime, QTimer
@@ -14,7 +13,7 @@ from asammdf.arloo.ui.report_dialog import Ui_report_dialog
 from asammdf.arloo.widgets.wysiwygedit import WysiwygEdit
 
 POUNDS_RATIO = 4.448
-GRAPH_HEIGHT = 400
+
 
 def to_number(value_str):
     return float(value_str)
@@ -82,10 +81,8 @@ class ReportDialog(Ui_report_dialog, QDialog):
         self.authorEdit.textEdited.connect(self.update_render_timer)
         self.dateEdit.dateChanged.connect(self.update_render_timer)
         self.descriptionEdit.textChanged.connect(self.update_render_timer)
-        self.sizeRatioBox.valueChanged.connect(self.update_render_timer)
-        self.keepAspectRatioCheckBox.stateChanged.connect(self.update_render_timer)
 
-        self.saveButton.clicked.connect(self.save_to)
+        self.saveButton.clicked.connect(self.save_to_pdf)
         self.closeButton.clicked.connect(self.reject)
         self.printButton.clicked.connect(self.printPage)
 
@@ -110,11 +107,6 @@ class ReportDialog(Ui_report_dialog, QDialog):
                                    ci_image=report_data.ci_image
                                    )
         self.web_view.setHtml(rendered)
-        nowstr = datetime.now().strftime("%H%M%S")
-        # downloads_path = str(Path.home() / "Downloads")
-        f = open(Path.joinpath(Path.home(),'Downloads','preview_'+nowstr+'.html'), 'w', encoding='utf-8')
-        f.write(rendered)
-        f.close()
 
     def accept(self) -> None:
         self.refresh_report_data()
@@ -134,10 +126,6 @@ class ReportDialog(Ui_report_dialog, QDialog):
         data.description = self.descriptionEdit.toHtml()
         data.ci_image = self._data_provider.ci_dataurl
         data.graph_image = self._data_provider.graph_dataurl
-
-        ratioValue = self.sizeRatioBox.value()
-        data.g_height = GRAPH_HEIGHT * ratioValue / 100
-        data.keep_aspect_ratio = self.keepAspectRatioCheckBox.isChecked()
         self.report_data = data
         return self.report_data
 
@@ -149,7 +137,7 @@ class ReportDialog(Ui_report_dialog, QDialog):
     def reject(self) -> None:
         super().reject()
 
-    def save_to(self) -> None:
+    def save_to_pdf(self) -> None:
         filter = "PDF files (*.pdf)"
         suffix = ".pdf"
 
